3dba9b6729ac6531ccfca5c9b6f7c40b771f605a
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-05-01 20:09:14 +0200

    Projektdateien hinzufügen.

---
+++ CustomDataTyp/Base64.cs
@@ -0,0 +1,162 @@
+//-----------------------------------------------------------------------
+// <copyright file="Base64.cs" company="Lifeprojects.de">
+//     Class: Base64
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+//
+// <Framework>8.0</Framework>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>01.05.2025 20:04:44</date>
+//
+// <summary>
+// Struct Class for Custom DataType Base64
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Console.CDT_Base64.Base
+{
+    using System;
+
+    public struct Base64 : IEquatable<Base64>, IComparable<Base64>
+    {
+        private readonly string _value;
+
+        public Base64(string value)
+        {
+            this._value = value;
+        }
+
+        public string Value
+        {
+            get
+            {
+                return this._value;
+            }
+        }
+
+        public static string Default
+        {
+            get { return string.Empty; }
+        }
+
+        public bool IsNullOrEmpty
+        {
+            get { return string.IsNullOrEmpty(this.Value); }
+        }
+
+        public int Length
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.Value) == true)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return this.Value.Length;
+                }
+            }
+        }
+
+        public bool IsNull
+        {
+            get
+            {
+                if (this.Value == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static implicit operator Base64(string value)
+        {
+            return new Base64(value);
+        }
+
+        public static implicit operator Base64(byte[] value)
+        {
+            if (value == null || value.Length == 0)
+            {
+                throw new ArgumentException("Der Wert für byte[] muß <> null bzw. größer 0 sein.");
+            }
+            else
+            {
+                string fromByteArray = Convert.ToBase64String(value);
+                return new Base64(fromByteArray);
+            }
+        }
+
+        public static bool operator ==(Base64 left, Base64 right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Base64 left, Base64 right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hashCode = new HashCode();
+            hashCode.Add(this.Value);
+            return hashCode.ToHashCode();
+        }
+
+        #region Implementation of IEquatable<Base64>
+        public override bool Equals(object obj)
+        {
+            if ((obj is Base64) == false)
+            {
+                return false;
+            }
+
+            Base64 other = (Base64)obj;
+            return Equals(other);
+        }
+
+        public bool Equals(Base64 other)
+        {
+            return this.Value == other.Value;
+        }
+        #endregion Implementation of IEquatable<Base64>
+
+        #region Konvertierung nach To...
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+
+        public byte[] ToByteArray()
+        {
+            if (string.IsNullOrEmpty(this.Value) == false)
+            {
+                return System.Convert.FromBase64String(this.Value);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion Konvertierung nach To...
+
+        #region Implementation of IComparable<Base64>
+
+        public int CompareTo(Base64 other)
+        {
+            int valueCompare = this.Value.CompareTo(other.Value);
+
+            return valueCompare;
+        }
+        #endregion Implementation of IComparable<Base64>
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+// <Template>
+// 	Version 2.0.2025.0, 28.4.2025
+// </Template>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>01.05.2025 20:01:09</date>
+//
+// <summary>
+// Konsolen Applikation mit Menü Demo zum Base64 DataTyp
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Console.CDT_Base64
+{
+    /* Imports from NET Framework */
+    using System;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Menüpunkt 1");
+                Console.WriteLine("2. Menüpunkt 2");
+                Console.WriteLine("X. Beenden");
+                Console.WriteLine("Wählen Sie einen Menüpunkt oder 'x' für beenden");
+                ConsoleKey key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.X)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    if (key == ConsoleKey.D1)
+                    {
+                        MenuPoint1();
+                    }
+                    else if (key == ConsoleKey.D2)
+                    {
+                        MenuPoint2();
+                    }
+                }
+            }
+            while (true);
+        }
+
+        private static void MenuPoint1()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+
+        private static void MenuPoint2()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+    }
+}
